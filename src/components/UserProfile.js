673318e8d25600187dc49773d32f0cc02d437b67
Dorcas Chepkoech <dorcaschepkoech717@gmail.com>
2025-10-28 20:54:34 +0300

    routes

---
+++ src/components/UserProfile.js
@@ -0,0 +1,19 @@
+import { useParams, useOutletContext } from "react-router-dom";
+
+function UserProfile() {
+  const { id } = useParams();
+  const users = useOutletContext();
+
+  const user = users.find(user => user.id === parseInt(id));
+
+  if (!user) return <h1>Loading...</h1>;
+
+  return (
+    <aside>
+      <h1>{user.name}</h1>
+      <p>Email: {user.email}</p>
+    </aside>
+  );
+}
+
+export default UserProfile;
